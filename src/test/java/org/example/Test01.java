dd441a63f09ba74a54f1e5208a465a5c42a94a88
yang <3440304752@qq.com>
2021-08-17 10:50:49 +0800

    修改Test01.java

--- src/test/java/org/example/Test01.java
+++ src/test/java/org/example/Test01.java
@@ -3,5 +3,7 @@ package org.example;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("test001");
+        System.out.println("test001");
+        System.out.println("test001");
     }
 }
