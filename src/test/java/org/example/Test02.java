f2174a28a1fd0cf4623b9f0f0762703d08302e32
yang <3440304752@qq.com>
2021-08-17 11:27:55 +0800

    C1客户端操作

--- src/test/java/org/example/Test02.java
+++ src/test/java/org/example/Test02.java
@@ -3,5 +3,6 @@ package org.example;
 public class Test02 {
     public static void main(String[] args) {
         System.out.println("test02");
+        System.out.println("C1客户端操作->test02");
     }
 }
